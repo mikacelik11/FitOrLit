27798334d19d535038567feac8e7e8eecbc1523a
MikaC11-bit <mikael_celik@hotmail.com>
2026-09-10 15:20:23 -0600

    fixed the onboarding logic and finally implemented my first test correctly

--- onboard.py
+++ onboard.py
@@ -7,7 +7,8 @@ class User:
 class UserStore:
     def __init__(self):
         self.users = []
-
+    # takes in a username and password and creates a user object and appends to list
+    # of users if the username is unique
     def register(self, username, password):
         for i in range(len(self.users)):
             if username in self.users[i].username:
@@ -16,21 +17,21 @@ class UserStore:
         self.users.append(User(username, password))
         return True
         
-        
+    # prompts the user to create an account and if the username is already taken it
+    # reprompts the user until they enter a valid unique username.   
     def signUp(self):
         while True:
             un = input("Make your username: ")
             pw = input("Make your password: ")
             
-            if self.register(un, pw) != True:
-                print("invalid username")
+            if self.register(un, pw):
+                print("account made")
                 return True
+            print("account already exists, try again")
             
 
-            return False
+           
         
-user_list = UserStore()
-user_list.signUp()
-user_list.signUp()
+
 
     
--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import unittest
-from FitOrLit.onboard import User
-from unittest.mock import Mock, patch
 
-class testClass:
-    def test_login_bool(self):
-        
-        User.input = lambda: 'mika', '123'
-        output = User.signUp()
-        assert output == 'expected_output'
-        
+from onboard import UserStore
+
+# This test is to check if register function works and a new user is registered
+def test_new_username_is_accepted(): 
+    store = UserStore()
+    assert store.register('mika', '123') is True
+
+# Now if there are duplicate username the person should be reprompted to make a different password
+# and should not be registered unless it is a unique username.  
+def test_duplicate_username_is_rejected():
+    store = UserStore()
+    store.register('mika', '123') 
+    assert store.register('mika', '1223') is False
         
     
